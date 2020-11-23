f262b59720967689a76ee1c654f0c4f1200815ca
P. J. Reed <preed@swri.org>
2020-11-23 17:07:22 -0600

    Work on ROS 2 support
    Signed-off-by: P. J. Reed <preed@swri.org>

--- swri_profiler/src/profiler.cpp
+++ swri_profiler/src/profiler.cpp
@@ -1,13 +1,28 @@
+#ifdef ROS2_BUILD
+#include <rclcpp/rclcpp.hpp>
+#else
 #include <ros/this_node.h>
-#include <swri_profiler/profiler.h>
 #include <ros/publisher.h>
+#endif
+#include <swri_profiler/profiler.h>
 
+#ifdef ROS2_BUILD
+#include <swri_profiler_msgs/msg/profile_data.hpp>
+#include <swri_profiler_msgs/msg/profile_data_array.hpp>
+#include <swri_profiler_msgs/msg/profile_index.hpp>
+#include <swri_profiler_msgs/msg/profile_index_array.hpp>
+#else
 #include <swri_profiler_msgs/ProfileIndex.h>
 #include <swri_profiler_msgs/ProfileIndexArray.h>
 #include <swri_profiler_msgs/ProfileData.h>
 #include <swri_profiler_msgs/ProfileDataArray.h>
+#endif
 
+#ifdef ROS2_BUILD
+namespace spm = swri_profiler_msgs::msg;
+#else
 namespace spm = swri_profiler_msgs;
+#endif
 
 namespace swri_profiler
 {
@@ -22,9 +37,15 @@ SpinLock Profiler::lock_;
 // variables instead we are able to keep more of the implementation
 // isolated.
 static bool profiler_initialized_ = false;
+static boost::thread profiler_thread_;
+#ifdef ROS2_BUILD
+std::shared_ptr<rclcpp::Node> swri_profiler::Profiler::node_;
+static std::shared_ptr<rclcpp::Publisher<spm::ProfileIndexArray> > profiler_index_pub_;
+static std::shared_ptr<rclcpp::Publisher<spm::ProfileDataArray> > profiler_data_pub_;
+#else
 static ros::Publisher profiler_index_pub_;
 static ros::Publisher profiler_data_pub_;
-static boost::thread profiler_thread_;
+#endif
 
 // collectAndPublish resets the closed_blocks_ member after each
 // update to reduce the amount of copying done (which might block the
@@ -32,6 +53,17 @@ static boost::thread profiler_thread_;
 // collected here in all_closed_blocks_;
 static std::unordered_map<std::string, spm::ProfileData> all_closed_blocks_;
 
+#ifdef ROS2_BUILD
+static rclcpp::Duration durationFromWall(const rclcpp::Duration &src)
+{
+  return rclcpp::Duration(src.seconds(), src.nanoseconds());
+}
+
+static rclcpp::Time timeFromWall(const rclcpp::Time &src)
+{
+  return rclcpp::Time(src.seconds(), src.nanoseconds());
+}
+#else
 static ros::Duration durationFromWall(const ros::WallDuration &src)
 {
   return ros::Duration(src.sec, src.nsec);
@@ -41,6 +73,7 @@ static ros::Time timeFromWall(const ros::WallTime &src)
 {
   return ros::Time(src.sec, src.nsec);
 }
+#endif
 
 void Profiler::initializeProfiler()
 {
@@ -48,11 +81,18 @@ void Profiler::initializeProfiler()
   if (profiler_initialized_) {
     return;
   }
-  
+
+#ifdef ROS2_BUILD
+  RCLCPP_INFO(node_->get_logger(), "Initializing swri_profiler...");
+  profiler_index_pub_ = node_->create_publisher<spm::ProfileIndexArray>("/profiler/index",
+                                                                        rclcpp::QoS(1).transient_local());
+  profiler_data_pub_ = node_->create_publisher<spm::ProfileDataArray>("/profiler/data", 100);
+#else
   ROS_INFO("Initializing swri_profiler...");
   ros::NodeHandle nh;
   profiler_index_pub_ = nh.advertise<spm::ProfileIndexArray>("/profiler/index", 1, true);
   profiler_data_pub_ = nh.advertise<spm::ProfileDataArray>("/profiler/data", 100, false);
+#endif
   profiler_thread_ = boost::thread(Profiler::profilerMain);   
   profiler_initialized_ = true;
 }
@@ -60,7 +100,12 @@ void Profiler::initializeProfiler()
 void Profiler::initializeTLS()
 {
   if (tls_.get()) {
-    ROS_ERROR("Attempt to initialize thread local storage again.");
+#ifdef ROS2_BUILD
+    RCLCPP_ERROR(node_->get_logger(),
+#else
+    ROS_ERROR(
+#endif
+      "Attempt to initialize thread local storage again.");
     return;
   }
 
@@ -77,6 +122,16 @@ void Profiler::initializeTLS()
 
 void Profiler::profilerMain()
 {
+#ifdef ROS2_BUILD
+  RCLCPP_DEBUG(node_->get_logger(), "swri_profiler thread started.");
+  rclcpp::Rate one_sec(1.0);
+  while (rclcpp::ok())
+  {
+    one_sec.sleep();
+    collectAndPublish();
+  }
+  RCLCPP_DEBUG(node_->get_logger(), "swri_profiler thread stopped.");
+#else
   ROS_DEBUG("swri_profiler thread started.");
   while (ros::ok()) {
     // Align updates to approximately every second.
@@ -85,20 +140,26 @@ void Profiler::profilerMain()
     (next-now).sleep();
     collectAndPublish();
   }
-  
   ROS_DEBUG("swri_profiler thread stopped.");
+#endif
 }
 
 void Profiler::collectAndPublish()
 {
   static bool first_run = true;
-  static ros::WallTime last_now = ros::WallTime::now();
-  
-  // Grab a snapshot of the current state.  
+  // Grab a snapshot of the current state.
   std::unordered_map<std::string, ClosedInfo> new_closed_blocks;
   std::unordered_map<std::string, OpenInfo> threaded_open_blocks;
+
+#ifdef ROS2_BUILD
+  static rclcpp::Time last_now = rclcpp::Clock().now();
+  rclcpp::Time now = rclcpp::Clock().now();
+  rclcpp::Time ros_now = rclcpp::Clock(RCL_ROS_TIME).now();
+#else
+  static ros::WallTime last_now = ros::WallTime::now();
   ros::WallTime now = ros::WallTime::now();
-  ros::Time ros_now = ros::Time::now();  
+  ros::Time ros_now = ros::Time::now();
+#endif
   {
     SpinLockGuard guard(lock_);
     new_closed_blocks.swap(closed_blocks_);
@@ -110,8 +171,13 @@ void Profiler::collectAndPublish()
 
   // Reset all relative max durations.
   for (auto &pair : all_closed_blocks_) {
+#ifdef ROS2_BUILD
+    pair.second.rel_total_duration = rclcpp::Duration(0);
+    pair.second.rel_max_duration = rclcpp::Duration(0);
+#else
     pair.second.rel_total_duration = ros::Duration(0);
     pair.second.rel_max_duration = ros::Duration(0);
+#endif
   }
 
   // Flag to indicate if a new item was added.
@@ -130,9 +196,11 @@ void Profiler::collectAndPublish()
     }
     
     all_info.abs_call_count += new_info.count;
-    all_info.abs_total_duration += durationFromWall(new_info.total_duration);
-    all_info.rel_total_duration += durationFromWall(new_info.rel_duration);
-    all_info.rel_max_duration = std::max(all_info.rel_max_duration,
+    all_info.abs_total_duration = durationFromWall(all_info.abs_total_duration) +
+      durationFromWall(new_info.total_duration);
+    all_info.rel_total_duration = durationFromWall(all_info.rel_total_duration) +
+      durationFromWall(new_info.rel_duration);
+    all_info.rel_max_duration = std::max(durationFromWall(all_info.rel_max_duration),
                                          durationFromWall(new_info.max_duration));
   }
   
@@ -145,11 +213,20 @@ void Profiler::collectAndPublish()
 
     size_t slash_index = threaded_label.find('/');
     if (slash_index == std::string::npos) {
-      ROS_ERROR("Missing expected slash in label: %s", threaded_label.c_str());
+#ifdef ROS2_BUILD
+      RCLCPP_ERROR(node_->get_logger(),
+#else
+      ROS_ERROR(
+#endif
+        "Missing expected slash in label: %s", threaded_label.c_str());
       continue;
     }
 
+#ifdef ROS2_BUILD
+    rclcpp::Duration duration = durationFromWall(now - threaded_info.t0);
+#else
     ros::Duration duration = durationFromWall(now - threaded_info.t0);
+#endif
     
     const auto label = threaded_label.substr(slash_index+1);
     auto &new_info = combined_open_blocks[label];
@@ -164,34 +241,47 @@ void Profiler::collectAndPublish()
     }
 
     new_info.abs_call_count++;
-    new_info.abs_total_duration += duration;
+    new_info.abs_total_duration = durationFromWall(new_info.abs_total_duration) + duration;
     if (first_run) {
-      new_info.rel_total_duration += duration;
+      new_info.rel_total_duration = durationFromWall(new_info.rel_total_duration) + duration;
     } else {
-      new_info.rel_total_duration += std::min(
+      new_info.rel_total_duration = durationFromWall(new_info.rel_total_duration) + std::min(
         durationFromWall(now - last_now), duration);
     }
-    new_info.rel_max_duration = std::max(new_info.rel_max_duration, duration);
+    new_info.rel_max_duration = std::max(durationFromWall(new_info.rel_max_duration), duration);
   }
 
   if (update_index) {
     spm::ProfileIndexArray index;
     index.header.stamp = timeFromWall(now);
+#ifdef ROS2_BUILD
+    index.header.frame_id = node_->get_name();
+#else
     index.header.frame_id = ros::this_node::getName();
+#endif
     index.data.resize(all_closed_blocks_.size());
     
-    for (auto const &pair : all_closed_blocks_) {
+    for (auto const &pair : all_closed_blocks_)
+    {
       size_t i = pair.second.key - 1;
       index.data[i].key = pair.second.key;
       index.data[i].label = pair.first;
-    }        
+    }
+#ifdef ROS2_BUILD
+    profiler_index_pub_->publish(index);
+#else
     profiler_index_pub_.publish(index);
+#endif
   }
 
   // Generate output message
   spm::ProfileDataArray msg;
   msg.header.stamp = timeFromWall(now);
+#ifdef ROS2_BUILD
+  msg.header.frame_id = node_->get_name();
+#else
   msg.header.frame_id = ros::this_node::getName();
+#endif
   msg.rostime_stamp = ros_now;
   
   msg.data.resize(all_closed_blocks_.size());
@@ -210,14 +300,20 @@ void Profiler::collectAndPublish()
     auto const &item = pair.second;
     size_t i = item.key - 1;
     msg.data[i].abs_call_count += item.abs_call_count;
-    msg.data[i].abs_total_duration += item.abs_total_duration;
-    msg.data[i].rel_total_duration += item.rel_total_duration;
+    msg.data[i].abs_total_duration = durationFromWall(msg.data[i].abs_total_duration) +
+      rclcpp::Duration(item.abs_total_duration);
+    msg.data[i].rel_total_duration = durationFromWall(msg.data[i].rel_total_duration) +
+      rclcpp::Duration(item.rel_total_duration);
     msg.data[i].rel_max_duration = std::max(
-      msg.data[i].rel_max_duration,
-      item.rel_max_duration);
+      durationFromWall(msg.data[i].rel_max_duration),
+      durationFromWall(item.rel_max_duration));
   }
-  
+
+#ifdef ROS2_BUILD
+  profiler_data_pub_->publish(msg);
+#else
   profiler_data_pub_.publish(msg);
+#endif
   first_run = false;
   last_now = now;
 }
--- swri_profiler_tools/src/register_meta_types.cpp
+++ swri_profiler_tools/src/register_meta_types.cpp
@@ -1,6 +1,11 @@
 #include <QMetaType>
+#ifdef ROS2_BUILD
+#include <swri_profiler_msgs/msg/profile_index_array.hpp>
+#include <swri_profiler_msgs/msg/profile_data_array.hpp>
+#else
 #include <swri_profiler_msgs/ProfileIndexArray.h>
 #include <swri_profiler_msgs/ProfileDataArray.h>
+#endif
 
 namespace swri_profiler_tools
 {
@@ -8,8 +13,13 @@ void registerMetaTypes()
 {
   // We need to register our ROS message types with Qt to be able to
   // pass them in Qt queued signals/slots (across threads).
+#ifdef ROS2_BUILD
+  qRegisterMetaType<swri_profiler_msgs::msg::ProfileIndexArray>("swri_profiler_msgs::msg::ProfileIndexArray");
+  qRegisterMetaType<swri_profiler_msgs::msg::ProfileDataArray>("swri_profiler_msgs::msg::ProfileDataArray");
+#else
   qRegisterMetaType<swri_profiler_msgs::ProfileIndexArray>("swri_profiler_msgs::ProfileIndexArray");
   qRegisterMetaType<swri_profiler_msgs::ProfileDataArray>("swri_profiler_msgs::ProfileDataArray");
+#endif
 }
 }  // namespace swri_profiler_tools
 
